d7684fd9371a8f4d3894f2d52bd2b40a73f1292f
Richard-LeBlanc <200182873@student.georgianc.on.ca>
2026-03-19 14:36:56 -0400

    Toggle tutorial menu on and off.

--- Assets/Game/Scripts/MenuManager.cs
+++ Assets/Game/Scripts/MenuManager.cs
@@ -1,24 +1,29 @@
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
     // based on lesson code
+    public GameObject tutorialMenu;
     public void StartGame()
     {
         SceneManager.LoadScene("GameScene"); // we have only 4 scenes, so no need to complicate this line
     }
 
-    public void ShowTutorial()
+    public void ToggleTutorial()
     {
-        
+        if (tutorialMenu != null)
+        {
+            tutorialMenu.SetActive(!tutorialMenu.activeSelf);
+        }
     }
 
     public void QuitGame()
     {
         Application.Quit();
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
-        #endif
+#endif
     }
 }
